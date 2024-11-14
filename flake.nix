7f52f018bdaf14feb3ab3c94188e6089914b19f9
Daniel Santa Cruz <dstcruz@gmail.com>
2024-11-14 07:52:15 -0700

    check point

--- flake.nix
+++ flake.nix
@@ -20,285 +20,304 @@
     };
   };
 
-  # Build using: 
+  # Build using:
   # > darwin-rebuild switch --flake .#daniel-macbook-pro-2018
   # Alternatively, since this system _is_ daniel-macbook-pro-2018, then we can build with:
   # > darwin-rebuild switch --flake .
-  outputs = inputs @ {lix-module, ...} : {
-    darwinConfigurations.daniel-macbook-pro-2018 =
-      inputs.nix-darwin.lib.darwinSystem {
-        system = "x86_64-darwin";
+  outputs = inputs @ {lix-module, ...}: {
+    darwinConfigurations.daniel-macbook-pro-2018 = inputs.nix-darwin.lib.darwinSystem {
+      system = "x86_64-darwin";
 
-        modules = [
-          # see: https://lix.systems/add-to-config/
-          lix-module.nixosModules.default
+      modules = [
+        # see: https://lix.systems/add-to-config/
+        lix-module.nixosModules.default
 
-          ({ pkgs, ... }: {
-            # List packages installed in system profile. To search by name, run:
-            # $ nix-env -qaP | grep wget
-            environment.systemPackages = [
-              # List of system wide packages to install
+        ({pkgs, ...}: {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            # List of system wide packages to install
+
+            # putting these shells here so that they show up in
+            # /run/current-system/sw/bin
+            # and therefore can easily be used by my terminals
+            pkgs.nushell
+            pkgs.fish
+          ];
+
+          fonts.packages = [
+            pkgs.meslo-lgs-nf
+            pkgs.commit-mono
+          ];
+
+          homebrew = {
+            enable = true;
+            onActivation.cleanup = "uninstall";
+            onActivation.upgrade = true;
+            global.autoUpdate = false;
+            global.brewfile = true;
+            taps = ["homebrew/cask"];
+            caskArgs.no_quarantine = true;
+            casks = [
+              "arc"
+              "firefox"
+              "microsoft-edge"
+              "obsidian"
+              "raycast"
+              "telegram"
+              "visual-studio-code"
+              "wezterm"
             ];
 
-            fonts.packages = [
-              pkgs.meslo-lgs-nf
-              pkgs.commit-mono
-            ];
-
-            homebrew = {
-              enable = true;
-              onActivation.cleanup = "uninstall";
-              onActivation.upgrade = true;
-              global.autoUpdate = false;
-              global.brewfile = true;
-              taps = [ "homebrew/cask" ];
-              caskArgs.no_quarantine = true;
-              casks = [
-                "arc"
-                "firefox"
-                "microsoft-edge"
-                "obsidian"
-                "raycast"
-                "telegram"
-                "visual-studio-code"
-                "wezterm"
-              ];
-
-              # See https://github.com/mas-cli/mas
-              # > mas list  # for installed apps
-              # > mas search --price # to search the app store
-              masApps = {
-                #"Microsoft Remote Desktop" = 1295203466;
-                Amphetamine = 937984704;
-                Bitwarden = 1352778147;
-                #WhatsApp = 1147396723;
-                WireGuard = 1451685025;
-                Pages = 409201541;
-                Numbers = 409203825;
-                "AdGuard for Safari" = 1440147259;
-              };
+            # See https://github.com/mas-cli/mas
+            # > mas list  # for installed apps
+            # > mas search --price # to search the app store
+            masApps = {
+              "Accelerate" = 1459809092;
+              "AdGuard for Safari" = 1440147259;
+              "Hidden Bar" = 1452453066;
+              Amphetamine = 937984704;
+              Bitwarden = 1352778147;
+              Numbers = 409203825;
+              OneTab = 1540160809;
+              Pages = 409201541;
+              WireGuard = 1451685025;
             };
+          };
 
-            environment.shells = [ pkgs.fish ];
-            programs.fish.enable = true;
+          # environment.shells = [ pkgs.fish pkgs.nushell ];
+          programs.fish.enable = true;
 
-            users.users.dansan = {
-              name = "dansan";
-              home = "/Users/dansan";
-            };
+          users.users.dansan = {
+            name = "dansan";
+            home = "/Users/dansan";
+          };
 
-            networking.hostName = "daniel-macbook-pro-2018";
+          networking.hostName = "daniel-macbook-pro-2018";
 
-            system.defaults.dock = {
-              autohide = true;
-              autohide-delay = 0.0;
-              autohide-time-modifier = 0.0;
-              mineffect = "scale";
-              mru-spaces = false;
-              orientation = "left";
-              show-recents = false;
-              showhidden = true;
-              tilesize = 40;
+          system.defaults.dock = {
+            autohide = true;
+            autohide-delay = 0.0;
+            autohide-time-modifier = 0.0;
+            mineffect = "scale";
+            mru-spaces = false;
+            orientation = "left";
+            show-recents = false;
+            showhidden = true;
+            tilesize = 40;
 
-              wvous-tl-corner = 1; # disabled
-              wvous-tr-corner = 1; # disabled
-              wvous-bl-corner = 1; # disabled
-              wvous-br-corner = 1; # disabled
-            };
+            wvous-tl-corner = 1; # disabled
+            wvous-tr-corner = 1; # disabled
+            wvous-bl-corner = 1; # disabled
+            wvous-br-corner = 1; # disabled
+          };
 
-            system.defaults.screencapture.type = "jpg";
+          system.defaults.screencapture.type = "jpg";
 
-            system.defaults.trackpad = {
-              Clicking = true;
-              TrackpadRightClick = true;
-              TrackpadThreeFingerDrag = true;
-            };
+          system.defaults.trackpad = {
+            Clicking = true;
+            TrackpadRightClick = true;
+            TrackpadThreeFingerDrag = true;
+          };
 
-            system.defaults.finder = {
-              AppleShowAllExtensions = true;
-              _FXShowPosixPathInTitle = true;
-              AppleShowAllFiles = true;
-              ShowPathbar = true;
-              ShowStatusBar = true;
-            };
+          system.defaults.finder = {
+            AppleShowAllExtensions = true;
+            _FXShowPosixPathInTitle = true;
+            AppleShowAllFiles = true;
+            ShowPathbar = true;
+            ShowStatusBar = true;
+          };
 
-            system.defaults.NSGlobalDomain = {
-              NSAutomaticSpellingCorrectionEnabled = false;
-              NSAutomaticQuoteSubstitutionEnabled = false;
-              NSAutomaticPeriodSubstitutionEnabled = false;
-              NSAutomaticDashSubstitutionEnabled = false;
-              NSAutomaticCapitalizationEnabled = false;
-            };
+          system.defaults.NSGlobalDomain = {
+            NSAutomaticSpellingCorrectionEnabled = false;
+            NSAutomaticQuoteSubstitutionEnabled = false;
+            NSAutomaticPeriodSubstitutionEnabled = false;
+            NSAutomaticDashSubstitutionEnabled = false;
+            NSAutomaticCapitalizationEnabled = false;
+          };
 
-            system.keyboard.enableKeyMapping = true;
-            system.keyboard.remapCapsLockToControl = true;
+          system.keyboard.enableKeyMapping = true;
+          system.keyboard.remapCapsLockToControl = true;
 
-            nix.useDaemon = true;
-            nix.extraOptions = ''
-              experimental-features = nix-command flakes
-            '';
+          nix.useDaemon = true;
+          nix.extraOptions = ''
+            experimental-features = nix-command flakes
+          '';
 
-            # Used for backwards compatibility, please read the changelog before changing.
-            # $ darwin-rebuild changelog
-            system.stateVersion = 4;
-          })
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 4;
+        })
 
-          inputs.home-manager.darwinModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
+        inputs.home-manager.darwinModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
 
-              users.dansan.imports = [
-                ({ pkgs, config, ... }: {
-                  # Don't change this when you change package inputs. Leave it alone.
-                  home.stateVersion = "23.05";
+            users.dansan.imports = [
+              ({
+                pkgs,
+                config,
+                ...
+              }: {
+                # Don't change this when you change package inputs. Leave it alone.
+                home.stateVersion = "23.05";
 
-                  # specify my home-manager configs
-                  home.packages = [
-                    # List of user-only packages to install
-                    # here are the ones that I don't care to tweak
-                    # otherwise they'd go under programs.<module>...
-                    (pkgs.ripgrep.override { withPCRE2 = true; })
-                    pkgs.atuin
-                    pkgs.binutils
-                    pkgs.clac
-                    pkgs.coreutils-prefixed
-                    pkgs.difftastic
-                    pkgs.editorconfig-core-c
-                    pkgs.emacs
-                    pkgs.fd
-                    pkgs.findutils
-                    pkgs.fishPlugins.tide
-                    pkgs.fontconfig
-                    pkgs.gawk
-                    pkgs.git
-                    pkgs.gnugrep
-                    pkgs.gnutls
-                    pkgs.home-manager
-                    pkgs.imagemagick
-                    pkgs.iosevka
-                    pkgs.ispell
-                    pkgs.moar
-                    pkgs.neofetch
-                    pkgs.neovim
-                    pkgs.nixfmt-classic
-                    pkgs.nushell
-                    pkgs.pandoc
-                    pkgs.riff
-                    pkgs.ripgrep
-                    pkgs.shellcheck
-                    pkgs.sqlite
-                    pkgs.tree-sitter
-                    pkgs.yazi
-                    pkgs.zstd
-                    pkgs.zoxide
-                  ];
+                # specify my home-manager configs
+                home.packages = [
+                  # List of user-only packages to install
+                  # here are the ones that I don't care to tweak
+                  # otherwise they'd go under programs.<module>...
+                  (pkgs.ripgrep.override {withPCRE2 = true;})
+                  pkgs.binutils
+                  pkgs.clac
+                  pkgs.coreutils-prefixed
+                  pkgs.difftastic
+                  pkgs.editorconfig-core-c
+                  pkgs.fd
+                  pkgs.findutils
+                  pkgs.fishPlugins.tide
+                  pkgs.fontconfig
+                  pkgs.gawk
+                  pkgs.git
+                  pkgs.gnugrep
+                  pkgs.gnutls
+                  pkgs.home-manager
+                  pkgs.imagemagick
+                  pkgs.iosevka
+                  pkgs.ispell
+                  pkgs.moar
+                  pkgs.neofetch
+                  pkgs.neovim
+                  pkgs.nixfmt-classic
+                  pkgs.pandoc
+                  pkgs.riff
+                  pkgs.shellcheck
+                  pkgs.sqlite
+                  pkgs.tree-sitter
+                  pkgs.zstd
+                ];
 
-                  home.sessionVariables = {
-                    CLICOLOR = 1;
-                    EDITOR = "nvim";
-                    TERMINAL = "fish";
-                    TERM = "xterm-256color";
+                home.sessionVariables = {
+                  CLICOLOR = 1;
+                  EDITOR = "nvim";
+                };
+
+                programs.atuin = {
+                  enable = true;
+                  enableFishIntegration = true;
+                  enableZshIntegration = true;
+                  enableNushellIntegration = true;
+                };
+
+                programs.carapace = {
+                  enable = true;
+                  enableNushellIntegration = true;
+                };
+
+                programs.eza = {
+                  enable = true;
+                  enableFishIntegration = true;
+                  #enableNushellIntegration = true;
+                };
+
+                programs.git = {
+                  enable = true;
+                  package = pkgs.git;
+                  #package = pkgs.gitAndTools.gitFull;
+                  userEmail = "dstcruz@gmail.com";
+                  userName = "Daniel Santa Cruz";
+                  difftastic.enable = true;
+                  aliases = {
+                    dtl = "difftool";
+                    dlog = "-c diff.external=difft log -p --ext-diff";
                   };
+                };
 
-                  programs.atuin = {
-                    enable = true;
-                    enableFishIntegration = true;
-                    enableZshIntegration = true;
-                  };
-                  
-                  programs.eza = {
-                    enable = true;
-                    enableFishIntegration = true;
-                  };
+                programs.nushell = {
+                  enable = true;
+                };
 
-                  programs.git = {
-                    enable = true;
-                    package = pkgs.git;
-                    #package = pkgs.gitAndTools.gitFull;
-                    userEmail = "dstcruz@gmail.com";
-                    userName = "Daniel Santa Cruz";
-                    difftastic.enable = true;
-                    aliases = {
-                      dtl = "difftool";
-                      dlog = "-c diff.external=difft log -p --ext-diff";
-                    };
-                  };
+                programs.fish = {
+                  enable = true;
 
-                  programs.fish = {
-                    enable = true;
-
-                    functions = {
-                      # $1 length is 2 -> `cd ../`, 3 -> `cd ../../`, etc
-                      __multicd = "echo cd (string repeat --count (math (string length -- $argv[1]) - 1) ../)";
-                      forward-single-char-or-forward-word = ''
-                        set -l line (commandline -L)
-                        set -l cmd (commandline)
-                        set -l cursor (commandline -C)
-                        if test (string length -- $cmd[$line]) = $cursor
-                          commandline -f forward-word
-                        else
-                          commandline -f forward-single-char
-                        end
-                      '';
-                    };
-
-                    plugins = [
-                      { name = "fishPlugins.tide"; src = pkgs.fishPlugins.tide.src; }
-                    ];
-
-                    shellAbbrs = 
-                      let
-                        global = {
-                          position = "anywhere";
-                        };
-                        cursor = {
-                          setCursor = true;
-                        };
-                        regex = pat: { regex = pat; };
-                        function = name: { function = name; };
-                        text = str: { expansion = str; };
-                      in {
-                        _dotdot =
-                          regex "^\\.\\.+$" // function "__multicd";
-                        calc = "clac";
-                      };
-
-                    interactiveShellInit = ''
-                      # disable greeting
-                      set -U fish_greeting
-
-                      # my bindings
-                      bind \cf forward-single-char-or-forward-word
+                  functions = {
+                    # $1 length is 2 -> `cd ../`, 3 -> `cd ../../`, etc
+                    __multicd = "echo cd (string repeat --count (math (string length -- $argv[1]) - 1) ../)";
+                    forward-single-char-or-forward-word = ''
+                      set -l line (commandline -L)
+                      set -l cmd (commandline)
+                      set -l cursor (commandline -C)
+                      if test (string length -- $cmd[$line]) = $cursor
+                        commandline -f forward-word
+                      else
+                        commandline -f forward-single-char
+                      end
                     '';
                   };
 
-                  programs.direnv = {
-                    enable = true;
-                    nix-direnv.enable = true;
+                  plugins = [
+                    {
+                      name = "fishPlugins.tide";
+                      src = pkgs.fishPlugins.tide.src;
+                    }
+                  ];
+
+                  shellAbbrs = let
+                    global = {
+                      position = "anywhere";
+                    };
+                    cursor = {
+                      setCursor = true;
+                    };
+                    regex = pat: {regex = pat;};
+                    function = name: {function = name;};
+                    text = str: {expansion = str;};
+                  in {
+                    _dotdot =
+                      regex "^\\.\\.+$" // function "__multicd";
+                    calc = "clac";
                   };
 
-                  programs.fzf.enable = true;
+                  interactiveShellInit = ''
+                    # disable greeting
+                    set -U fish_greeting
 
-                  programs.yazi = {
-                    enable = true;
-                  };
+                    # my bindings
+                    bind \cf forward-single-char-or-forward-word
+                  '';
+                };
 
-                  programs.zoxide = {
-                    enable = true;
-                  };
+                programs.direnv = {
+                  enable = true;
+                  nix-direnv.enable = true;
+                };
 
-                  # ~/.config symlinks
-                  xdg.configFile = { };
+                programs.fzf.enable = true;
 
-                  # ~ symlinks
-                  home.file = { };
-                })
-              ];
-            };
-          }
-        ];
-      };
+                programs.yazi = {
+                  enable = true;
+                  enableFishIntegration = true;
+                  enableNushellIntegration = true;
+                };
+
+                programs.zoxide = {
+                  enable = true;
+                  enableFishIntegration = true;
+                  enableNushellIntegration = true;
+                };
+
+                # ~/.config symlinks
+                xdg.configFile = {};
+
+                # ~ symlinks
+                home.file = {};
+              })
+            ];
+          };
+        }
+      ];
+    };
   };
 }
