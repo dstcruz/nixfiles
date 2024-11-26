8b1892321d5719f612389dbcdb241b2690da5507
Daniel Santa Cruz <dstcruz@gmail.com>
2024-11-25 19:22:46 -0700

    check point

--- flake.nix
+++ flake.nix
@@ -273,9 +273,9 @@
 
                     $env.PROMPT_COMMAND_RIGHT = "" 
 
-                    $env.PROMPT_INDICATOR = "〉"
+                    $env.PROMPT_INDICATOR = "> "
                     $env.PROMPT_INDICATOR_VI_INSERT = ": "
-                    $env.PROMPT_INDICATOR_VI_NORMAL = "〉"
+                    $env.PROMPT_INDICATOR_VI_NORMAL = "> "
                     $env.PROMPT_MULTILINE_INDICATOR = "::: "
                   '';
                 };
